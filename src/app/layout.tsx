71a9b5422aba27396144e659afdadab73005ea65
hyunyikim <hailey@mass-adoption.com>
2024-11-17 23:08:29 +0100

    feat: change font to Bitter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from 'next';
+import { Bitter } from 'next/font/google';
+
 import './globals.css';
+
+const bitter = Bitter({
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: 'mypaperwork',
@@ -12,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={bitter.className}>
       <body>{children}</body>
     </html>
   );
