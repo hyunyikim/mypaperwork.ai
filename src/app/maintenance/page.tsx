877050253f7c743d4eb94c61efb918703d3541db
hyunyikim <hailey@mass-adoption.com>
2024-11-17 22:59:37 +0100

    feat: add style

--- src/app/maintenance/page.tsx
+++ src/app/maintenance/page.tsx
@@ -4,14 +4,15 @@ import Image from 'next/image';
 export default function Maintenance() {
   return (
     <div className="flex flex-col h-screen">
-      <header className="flex items-center h-20 py-4 px-8">
+      <header className="flex items-center h-20 lg:h-24 py-4 px-8">
         <Image src="/logo.svg" alt="logo" width={135} height={64} priority />
       </header>
-      <main className="flex flex-1 flex-col-reverse lg:flex-row items-center justify-center p-8 lg:p-24">
-        <section className="flex flex-col">
-          <h1>Under Maintenance</h1>
-          <p>
-            We are hard at work upgrading our technology and features to save your time. We’ll be back by <MaintenanceDate /> with some awesome updates.
+      <main className="flex flex-1 flex-col-reverse lg:flex-row items-center justify-center gap-2 lg:gap-4 p-8 lg:p-24">
+        <section className="flex flex-col gap-4 lg:gap-8">
+          <h1 className="text-3xl md:text-5xl font-semibold">Under Maintenance</h1>
+          <p className="text-lg md:text-xl">
+            We are hard at work upgrading our technology and features to save your time. <br />
+            We’ll be back by <MaintenanceDate /> with some awesome updates.
             <br />
             Thank you for your understanding.
           </p>
